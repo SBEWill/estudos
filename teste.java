29d910382fae5f4ff4ee4e523c97cb500ef9074e
SBEWill <166247887+SBEWill@users.noreply.github.com>
2025-04-23 20:12:05 -0300

    exercio_for.java

--- teste.java
+++ teste.java
@@ -1,3 +1,7 @@
+// exrcio sobre for - Leia um valor inteiro X (1 <= X <= 1000). Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o
+X, se for o caso.
+
+
 package estudando;
 
 import java.util.Scanner;
